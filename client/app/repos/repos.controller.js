c9c5ac1b37b149191dbf9f380532268bf71f9b48
Braun Goodson <braungoodson@gmail.com>
2015-05-21 19:02:53 -0400

    added depdnencies

--- client/app/repos/repos.controller.js
+++ client/app/repos/repos.controller.js
@@ -1,5 +1,9 @@
 'use strict';
 
 angular.module('ginApp')
-  .controller('ReposCtrl', function ($scope) {
+  .controller('ReposCtrl', function ($scope, $stateParams, ReposService) {
+    $scope.gin = $stateParams.gin;
+    $scope.repos = ReposService.get({
+      gin: $stateParams.gin
+    });
   });
