76d0ecf4e2945234c10db6da17f07be28ebc054a
Braun Goodson <braungoodson@gmail.com>
2015-05-21 19:02:20 -0400

    added action

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,10 +2,4 @@
 
 angular.module('ginApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
   });
