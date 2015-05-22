bf90cf5028d7e07a19c0919f5c77a2dc4620c8a7
Braun Goodson <braungoodson@gmail.com>
2015-05-21 22:24:58 -0400

    fixed service

--- client/app/issues/issues.controller.js
+++ client/app/issues/issues.controller.js
@@ -4,7 +4,7 @@ angular.module('ginApp')
   .controller('IssuesCtrl', function ($scope, $stateParams, IssuesService) {
     $scope.gin = $stateParams.gin;
     $scope.repo = $stateParams.repo;
-    $scope.issues = Issues.get({
+    $scope.issues = IssuesService.get({
       gin: $scope.gin,
       repo: $scope.repo
     });
